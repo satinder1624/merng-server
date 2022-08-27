98fa084547c7c78ccbb1cb4e9cc5d8624bf91ef0
satinder <satinder1624@gmail.com>
2022-08-27 18:49:43 -0400

    added server

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://satinder:JKA4jb6vrmnvOaRS@cluster0.0enwkv4.mongodb.net/merng?retryWrites=true&w=majority",
+  SECRET_KEY: "some secret value here",
+};
